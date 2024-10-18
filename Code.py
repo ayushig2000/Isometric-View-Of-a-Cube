908b8360b90d5459d89777b53a1445ed20cd015c
aygoyal <v-aygoyal@microsoft.com>
2024-10-18 22:05:54 +0530

    numpy

--- Code.py
+++ Code.py
@@ -2,8 +2,9 @@ import pyautogui
 import math
 import time
 import pandas as pd
+import numpy as np
 
-time.sleep(6) #this is a time it takes to start
+time.sleep(6) #tgithis is a time it takes to start
 
 
 a=210 #side length of a cube
